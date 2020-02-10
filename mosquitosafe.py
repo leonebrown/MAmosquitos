611fdcbb7556fd75d8f4d5d120c234b52f3e0820
leonebrown <izzycutty@gmail.com>
2020-02-10 02:25:54 -0500

    Update mosquitosafe.py

--- mosquitosafe.py
+++ mosquitosafe.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut
-
+import datetime
+from sklearn.linear_model import LogisticRegression
+import pickle
 
 st.title('Mosquito-borne disease risk assessor')
 st.write("User-friendly web application to assess seasonal mosquito-borne disease risk across Massachusetts")
 
-user_input = st.text_input("Search town, e.g., 'Somerville, MA'", "Somerville, MA")
+user_input = st.text_input("Search town, e.g., 'Somerville'", "Somerville")
 geolocator = Nominatim(user_agent="my-application")
 
 def do_geocode(user_input):
@@ -23,8 +26,6 @@ print('Coordinates: ', location.latitude, location.longitude)
 st.write("Found: ", location)
 st.write('Coordinates: ', location.latitude, location.longitude)
     
-#except:
-#   st.write("Couldn't find this location. Try a different town name?") #   location = geolocator.geocode('Burlington, Vermont')
 x = location.longitude
 y = location.latitude
 
@@ -33,11 +34,6 @@ disease = ['West Nile virus','Eastern Equine Encephalitis']
 moslist = ['January','February','March','April','May','June','July','August','September','October','November','December']
 
 datelist = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
-
-#df
-
-#user_input2 = st.text_input("Enter date of interest as month-day")
-#st.write(user_input2)
 
 option1 = st.selectbox(
     'Search Disease',
@@ -53,27 +49,71 @@ option3 = st.selectbox(
 
 ##################################################################
 # Run ML model
-df3 = pd.read_csv("/Users/lbrown01/Dropbox/DataScienceStuff/Data/WNV_EE/MLscaffolddf.csv")
+#read in landcover data to get land cover for each town
+#user will input a town, and for the prediction, pull out landcover values that match that town
+LCdat2011 = pd.read_csv("townLC2011fin_2.csv")
 
-from sklearn.model_selection import train_test_split
-X = df3.drop(['ResVal'], axis = 1) #axis = 1 for columns; need to drop response because I don't want to predict on response or get perfect model
-y = df3['ResVal']
+#read in climate data x town x day of year (averaged over the past 5 years)
+climdat = pd.read_csv("predDFclim2.csv")
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
+# load the model from disk
+if option1 == 'Eastern Equine Encephalitis' :
+    loaded_model = pickle.load(open('logreg_EEEmod.sav','rb'))
+else:
+    loaded_model = pickle.load(open('logreg_WNVmod.sav','rb'))
 
-from sklearn.linear_model import LogisticRegression
-logmodel = LogisticRegression()
-logmodel.fit(X_train,y_train)
+inputdate = option2 + " " + option3 + " " + "2020"
 
-prediction = logmodel.predict(X_test)
+#get date and convert to day of year
+#date_object = datetime.strptime(str, '%m/%d/%y')
+dateTime = datetime.datetime.strptime(inputdate, "%B %d %Y")
+DOY = dateTime.strftime('%j')
 
-from sklearn.metrics import classification_report
-print(classification_report(y_test,prediction))  #very bad because too few 0s
+#filter landcover dataframe to only have town = userinput
+LCtemp = LCdat2011[LCdat2011['town']==user_input.upper()]
 
-#day 193 is July 12
-d = {'POP2010': [81360], 'DOY': [193]}
+cc = LCtemp.at[1,'RPcultcrop']
+ss = LCtemp.at[1,'RPshrubscrub']
+do = LCtemp.at[1,'RPdevopen']
+dl = LCtemp.at[1,'RPdevlow']
+dm = LCtemp.at[1,'RPdevmed']
+dh = LCtemp.at[1,'RPdevhigh']
+gh = LCtemp.at[1,'RPgrassherb']
+ph = LCtemp.at[1,'RPpasturehay']
+ow = LCtemp.at[1,'RPopenwater']
+ehw = LCtemp.at[1,'RPemergherbwet']
+ww = LCtemp.at[1,'RPwoodywet']
+dfor = LCtemp.at[1,'RPdecidforest']
+efor = LCtemp.at[1,'RPevergrnfor']
+mfor = LCtemp.at[1,'RPmixedforest']
+
+#filter climate dataframe to only have town = userinput
+cltemp = climdat[climdat['town']==user_input]
+
+cltemp['DOY'] = cltemp['DOY'].apply(str)
+cltemp['DOY'] = cltemp['DOY'].str.zfill(3)
+
+ind = cltemp.loc[cltemp['DOY']== DOY]
+
+T7 = ind.iat[0,2]
+T14 = ind.iat[0,3]
+T21 = ind.iat[0,4]
+p7 = ind.iat[0,5]
+p14 = ind.iat[0,6]
+p21 = ind.iat[0,7]
+
+d = {'DOY':[DOY],'RPcultcrop':[cc],'RPshrubscrub':[ss],'RPdevopen':[do],
+       'RPdevlow':[dl],'RPdevmed':[dm],'RPdevhigh':[dh],'RPgrassherb':[gh],'RPpasturehay':[ph],
+       'RPopenwater':[ow],'RPemergherbwet':[ehw],'RPwoodywet':[ww],'RPdecidforest':[dfor],
+       'RPevergrnfor':[efor],'RPmixedforest':[mfor],'ppt7':[p7],'ppt14':[p14],'ppt21':[p21],'avgT7':[T7],'avgT14':[T14],'avgT21':[T21]}
+
 dfpredtest = pd.DataFrame(data=d)
+dog = loaded_model.predict(dfpredtest)
 
-dog = logmodel.predict(dfpredtest)
+np.set_printoptions(formatter={'float': lambda x: "{0:0.2f}".format(x)})
 
-st.write("Your WNV risk score is ", print(dog))
+st.title("Your local WNV risk per mosquito is", print(dog))
+
+st.write("Your local risk is calculated based on the number of mosquitos that have tested positive in your area over the past 16 years, as well as climate and land cover variables that affect infection rate in mosquitos. This estimate is the probability that any given mosquito biting you has West Nile virus (WNv) Eastern Equine Encephalitis (EEE). WNv and EEE are rare; for West Nile virus in particular, approximately 80% of infected people never show symptoms. Those that do show symptomes are typically those over the age of 40-50. If you are over the age of 60 you are at greatest risk. Please see https://www.cdc.gov/features/westnilevirus/index.html for more information.")
+
+st.write("Disclaimer: This tool is not meant to provide a medical evaluation or replace the advice of a medical professional. This tool was implemented using a logistic regression machine learning algorithm as part of the developer's participation in the Insight Health Data Science program in Boston, Massachusetts, USA (https://www.insighthealthdata.com). Please use this tool at your own discretion, and always protect yourself from biting insects and other animals that may be vectors of zoonotic diseases.")
